a136c992d65a4f0e5dd1264da83511156d33821d
zachvalenta <zjv5002@gmail.com>
2018-09-03 15:05:00 -0400

    add pytest fixture

--- test_pytest.py
+++ test_pytest.py
@@ -1,7 +1,18 @@
+import pytest
+
 from phonebook import Phonebook
 
 
-def test_add_and_lookup_entry():
-	phonebook = Phonebook()
+@pytest.fixture
+def phonebook():
+	return Phonebook()
+
+
+def test_add_and_lookup_entry(phonebook):
 	phonebook.add('alice', '12345')
 	assert "12345" == phonebook.lookup('alice')
+
+
+def test_missing_entry_raises_KeyError(phonebook):
+	with pytest.raises(KeyError):
+		phonebook.lookup('missing')
