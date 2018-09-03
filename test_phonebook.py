5ed4e7d485814c1ab1175f0cab24b44d75c51262
zachvalenta <zjv5002@gmail.com>
2018-09-02 21:10:15 -0400

    add fixture

--- test_phonebook.py
+++ test_phonebook.py
@@ -5,15 +5,13 @@ from phonebook import Phonebook
 
 class PhonebookTest(unittest.TestCase):
 
-	def test_create_phonebook(self):
-		phonebook = Phonebook()
+	def setUp(self):
+		self.phonebook = Phonebook()
 
 	def test_lookup_entry_by_name(self):
-		phonebook = Phonebook()
-		phonebook.add("alice", "12345")
-		self.assertEqual("12345", phonebook.lookup("alice"))
+		self.phonebook.add("alice", "12345")
+		self.assertEqual("12345", self.phonebook.lookup("alice"))
 
 	def test_miss_entry_raises_KeyError(self):
-		phonebook = Phonebook()
 		with self.assertRaises(KeyError):
-			phonebook.lookup('missing')
+			self.phonebook.lookup('missing')
